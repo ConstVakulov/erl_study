013e501b2315e12058fd1d33a19556f2dc8af1c9
ConstVakulov <39188447+ConstVakulov@users.noreply.github.com>
2018-05-14 17:36:22 +0300

    Add files via upload

--- kitchen.erl
+++ kitchen.erl
@@ -1,22 +1,32 @@
 -module(kitchen).
--export([fridge/0,fridge2/1]).
+-export([fridge1/0]).
+-export([fridge2/1]).
 
-fridge() ->
+fridge1() ->
 	receive
 		{From,{store,Food}} ->
-			From ! {polozhil, Food},
-			fridge();
+			From ! {self(),{polozhil, Food}},
+			fridge1();
 		{From,{take,_Food}} ->
-			From ! brat_nechego,
-			fridge();
+			From ! {self(),brat_nechego},
+			fridge1();
 		_ -> kakaya_to_huinya
 	end.
 	
 fridge2(FoodList) ->
 	receive
 		{From,{store, Food}} ->
-			From ! {polozhil, Food},
+			From ! {self(),{polozhil, Food}},
 			fridge2([Food|FoodList]);
 		{From, {take, Food}} ->
 			case lists:member(Food, FoodList) of
+				true ->
+					From ! {self(),{vzyal,Food}},
+					fridge2(lists:delete(Food,FoodList));
+				false ->
+					From ! {self(),not_found},
+					fridge2(FoodList)
+			end;
+		{From, _} -> From ! {self(),FoodList}
+	end.
 			
